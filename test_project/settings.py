6744eccb7c72b9bd77e601435ad436b0cdaa81e9
marnikvla <condor52@yandex.ru>
2019-04-04 11:31:03 +0300

    ВТОРОЙ коммит

--- test_project/settings.py
+++ test_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'test_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'pages/templates/pages')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
